d196174c963fddfa5817fbaff7c2ba17411eadda
Kim-min-kyoung <alsrudghtkd@naver.com>
2022-03-02 21:20:52 +0900

    css 수정

--- include/header.php
+++ include/header.php
@@ -12,13 +12,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../etc/favicon.ico" type="image/x-icon" sizes="16x16">
     <link rel="stylesheet" href="http://use.fontawesome.com/releases/v5.11.2/css/all.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/product.css?after">
     <link rel="stylesheet" href="../css/login.css?after">
-    <link rel="stylesheet" href="../css/join.css?after">
+    <link rel="stylesheet" href="../css/join.css">
     <link rel="stylesheet" href="../css/about.css">
     <link rel="stylesheet" href="../css/qna.css?after">
     <link rel="stylesheet" href="../css/cart.css">
